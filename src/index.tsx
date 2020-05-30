6a2d0f48c25cecd3034b08b140dc7fba76fe7143
Ajeet <ajeets.shah@hotmail.com>
2020-05-30 13:30:10 +0530

    remove i18n

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,20 @@ import App from './app/app'
 import store from './store/store'
 import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
-import { IntlProvider } from 'react-intl'
-import messages from './translations/messages'
+// import { IntlProvider } from 'react-intl'
+// import messages from './translations/messages'
 // import serialize from 'serialize-javascript'
 
-const language = navigator.language.split(/[-_]/)[0]
+// const language = navigator.language.split(/[-_]/)[0]
 
 // console.log(serialize(window['SERVER_DATA'])) // Using server data after serializing
 
 ReactDOM.render(
-  <IntlProvider defaultLocale="en" locale={language} messages={messages[language]}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </IntlProvider>,
+  // <IntlProvider defaultLocale="en" locale={language} messages={messages[language]}>
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  // </IntlProvider>,
   document.getElementById('root')
 )
 
